fc76c6983d1614d0b50e5e128385b2d794a1ab6b
R0p3sh <rup3shrj@gmail.com>
2026-01-28 23:34:33 +0545

    more object added

--- script/index.js
+++ script/index.js
@@ -1,30 +1,22 @@
 let itemscontainerElement = document.querySelector(".items-container");
-let item = {
-  item_image: 'images/1.jpg',
-  rating: {
-    stars: 4.5,
-    noOfReviews: 1400,
-  },
-  company_name: 'Carlton London',
-  item_name: 'Rhodium-Plated CZ Floral Studs',
-  current_price: 606,
-  original_price: 1045,
-  discount: 42,
-};
 
-itemscontainerElement.innerHTML = `
-  <div class="item-container">
-      <img class = "item-image"src="${item.item_image}" alt="item image">
+let innerHtml = '';
+items.forEach(item =>{
+  innerHtml +=   
+  `<div class="item-container">
+      <img class = "item-image"src="${item.image}" alt="item image">
       <div class = "rating">
-          ${item.rating.stars}⭐️ | ${item.rating.noOfReviews}
+          ${item.rating.stars}⭐️ | ${item.rating.count}
       </div>
-      <div class="company-name">${item.company_name}</div>
+      <div class="company-name">${item.company}</div>
       <div class = "item-name">${item.item_name}</div>
       <div class="price">
           <span class="current-price"> Rs ${item.current_price}</span>
           <span class="original-price"> Rs ${item.original_price}</span>
-          <span class="discount">(${item.discount}% OFF)</span>
+          <span class="discount">(${item.discount_percentage}% OFF)</span>
       </div>
       <button class = "btn-add-bag"> Add to Bag</button>
-  </div>  
-`;
+   </div>`  
+});
+
+itemscontainerElement.innerHTML = innerHtml;
